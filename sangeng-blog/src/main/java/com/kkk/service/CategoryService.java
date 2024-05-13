534d5464d54799f3935b9a325571188745707c0e
lonelykkk <2765314967@qq.com>
2024-05-13 20:45:08 +0800

    更新首页分页查询功能

--- sangeng-blog/src/main/java/com/kkk/service/CategoryService.java
+++ sangeng-blog/src/main/java/com/kkk/service/CategoryService.java
@@ -3,6 +3,7 @@ package com.kkk.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.kkk.domain.entity.CategoryEntity;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,5 +14,6 @@ import java.util.Map;
  * @date 2024-05-11 09:06:25
  */
 public interface CategoryService extends IService<CategoryEntity> {
+    List<CategoryEntity> getCategoryList();
 }
 
