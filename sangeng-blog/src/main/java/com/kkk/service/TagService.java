3a335ca95e0debfb69c6d0ac4ff829a1746ee798
lonelykkk <2765314967@qq.com>
2024-05-13 20:45:08 +0800

    更新首页分页查询功能

--- sangeng-blog/src/main/java/com/kkk/service/TagService.java
+++ sangeng-blog/src/main/java/com/kkk/service/TagService.java
@@ -3,14 +3,13 @@ package com.kkk.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.kkk.domain.entity.TagEntity;
 
-import java.util.Map;
 
 /**
  * 标签
  *
  * @author lonelykkk
- * @email 2765314967@qq.com
- * @date 2024-05-11 09:06:25
+ * &#064;email  2765314967@qq.com
+ * &#064;date  2024-05-11 09:06:25
  */
 public interface TagService extends IService<TagEntity> {
 
---
+++ sangeng-blog/src/main/java/com/kkk/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,36 @@
+package com.kkk.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.kkk.domain.entity.CategoryEntity;
+import com.kkk.mapper.CategoryDao;
+import com.kkk.service.ArticleService;
+import com.kkk.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Service("categoryService")
+@Slf4j
+public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
+
+    @Autowired
+    private ArticleService articleService;
+
+    /**
+     * 查询分类列表
+     * @return
+     */
+    @Override
+    public List<CategoryEntity> getCategoryList() {
+        /**
+         * 将所有分类类型返回
+         */
+
+        final long id = Thread.currentThread().getId();
+        return list();
+    }
+}
