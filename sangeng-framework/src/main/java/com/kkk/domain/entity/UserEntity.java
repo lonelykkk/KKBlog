2c5abb9bd2702935007d301a3010adbb787f0d90
lonelykkk <2765314967@qq.com>
2024-05-14 20:53:12 +0800

    更新多级评论

--- sangeng-framework/src/main/java/com/kkk/domain/entity/UserEntity.java
+++ sangeng-framework/src/main/java/com/kkk/domain/entity/UserEntity.java
@@ -16,7 +16,7 @@ import lombok.Data;
  */
 @Data
 @TableName("sys_user")
-public class SysUserEntity implements Serializable {
+public class UserEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	/**
